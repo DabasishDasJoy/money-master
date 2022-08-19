c0a63ff1a73799a4bbfcdbf8ef1e1f1201470d4e
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-08-19 23:03:38 +0600

    all calculation completed

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
     const balance = getInputFieldValueById('income') - getElementValueById('total-expense');
     document.getElementById('balance').innerText = balance;
 });
+
+document.getElementById('btn-save').addEventListener('click', function(){
+
+    document.getElementById('saving-amount').innerText = getInputFieldValueById('income') * (getInputFieldValueById('save')/100);
+
+    document.getElementById('remaining-balance').innerText = getElementValueById('balance') - (getInputFieldValueById('income') * (getInputFieldValueById('save')/100));
+});
